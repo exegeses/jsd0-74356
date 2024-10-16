52f7971b08cba4e1f862ab0ba2eefe6f19802039
Marcos <exegeses@gmail.com>
2024-10-16 00:05:02 -0300

    Trabajo con variables y constantes | Escribir dentro de un elemento HTML

---
+++ clase2/js/usuario.js
@@ -0,0 +1,10 @@
+/* 
+    - Pedimos al usuario que ingrese su nombre
+    - Una vez que el usuario ingresa su nombre 
+      almacenamos ese dato en memoria
+    - Escribimos el nombre del usuario en el span
+*/
+let usuario = prompt('ingrese su nombre');
+//document.write(usuario);
+// escribimos en el span
+frase.innerText = usuario;
---
+++ clase2/js/variables.js
@@ -0,0 +1,26 @@
+/*
+    Para declarar una variable en JavaScript 
+    utilizamos la palabra reservada 'let'
+*/
+let numero = 0;
+let frase = 'hola mundo desde javascript';
+let rojo = 'hsl(0, 50%, 50%)';
+/*
+    Para declarar una constante en JavaScript 
+    utilizamos la palabra reservada 'const'
+*/
+const PI = 3.141592;
+
+
+// escribimos en el documento
+document.write( frase );
+document.write( '<br>' );
+
+// sobreescribimos el valor de la variable frase
+frase = 'JavaScript es muy fácil';
+// sobreescribimos el valor de la constante PI
+//PI = 73; No se puede sobre escribir una constante
+
+
+// escribimos en el documento
+document.write( frase );
